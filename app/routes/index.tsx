6f2daa57076e94eba9d9be427b2d3e5c1acdc066
Wade Baglin <wade.baglin@makerx.com.au>
2023-04-17 11:23:24 +1000

    chore: simplify for blogging purposes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -9,27 +9,39 @@ import { randomDelayBetween } from '~/utilities/delay'
 import { GlassButton } from '~/components/molecules/GlassButton'
 import { Title } from '~/components/atoms/Title'
 import { GlassPanel } from '~/components/molecules/GlassPanel'
-import {
-  completeTodoItemValidationSchema,
-  deleteTodoItemValidationSchema,
-  TodoItem,
-} from '~/components/organisms/TodoItem'
 import { Panel } from '~/components/atoms/Panel'
-import {
-  UpsertTodo,
-  upsertTodoValidationSchema,
-} from '~/components/organisms/UpsertTodo'
 import { useLoadingContext } from '~/contexts/loadingContext'
 import Loading from 'icon/LoadingIndicator'
+import { zfd } from 'zod-form-data'
+import {
+  ValidatedCheckboxInput,
+  ValidatedHiddenInput,
+  ValidatedTextInput,
+} from '~/components/atoms/ValidatedInput'
+import { cn } from '~/utilities/cn'
+import { IconButton } from '~/components/molecules/IconButton'
+import Delete from '../../icon/Delete'
+import Edit from '../../icon/Edit'
+import { Button } from '~/components/atoms/Button'
 
 const validator = withZod(
   z.discriminatedUnion('_action', [
     z.object({
       _action: z.literal('reset'),
     }),
-    upsertTodoValidationSchema,
-    deleteTodoItemValidationSchema,
-    completeTodoItemValidationSchema,
+    z.object({
+      _action: z.literal('upsert'),
+      description: z.string().min(2).max(50),
+      id: zfd.numeric(z.number().optional()),
+    }),
+    z.object({
+      _action: z.literal('delete'),
+      id: zfd.numeric(),
+    }),
+    z.object({
+      _action: z.literal('complete'),
+      id: zfd.numeric(),
+    }),
   ]),
 )
 
@@ -44,12 +56,10 @@ export const action = async (data: DataFunctionArgs) => {
   const formData = await data.request.formData()
   const result = await validator.validate(formData)
 
-  console.log(result)
-
   if (result.error) return validationError(result.error)
 
   switch (result.data._action) {
-    case 'reset':    {
+    case 'reset': {
       await db.populateSample()
       break
     }
@@ -64,7 +74,9 @@ export const action = async (data: DataFunctionArgs) => {
     case 'upsert': {
       const isEdit = !isNaN(Number(result.data.id))
       if (isEdit) {
-        db.patch(Number(result.data.id), { description: result.data.description })
+        db.patch(Number(result.data.id), {
+          description: result.data.description,
+        })
       } else {
         db.append({ description: result.data.description })
       }
@@ -108,22 +120,83 @@ export default function Index() {
         />
         <Panel className='mt-2 px-4' aria-live='polite'>
           {todos.map((td, i) => (
-            <TodoItem
-              key={`todo-${td.id}`}
-              todo={td}
-              onEdit={setEdit}
-              disableActions={Boolean(edit)}
-              disabled={loadingContext.isLoading}
-            />
+            <ValidatedForm key={td.id} validator={validator} method='post'>
+              <ValidatedHiddenInput name='id' value={td.id.toString()} />
+              <Panel
+                border='b'
+                className={cn('p-3', 'hover:bg-glass/20', 'grid grid-flow-col')}
+              >
+                <div
+                  aria-label={`To-do entry ${td.description}`}
+                  aria-flowto={`delete-${td.id}`}
+                >
+                  {td.description}
+                </div>
+                {!Boolean(edit) && (
+                  <div className='w-30 justify-self-end grid gap-2 grid-flow-col content-center'>
+                    <IconButton
+                      id={`delete-${td.id}`}
+                      color='Red'
+                      type='submit'
+                      name='_action'
+                      value='delete'
+                      disabled={loadingContext.isLoading}
+                      aria-label='Delete to-do entry'
+                    >
+                      <Delete aria-hidden={true} />
+                    </IconButton>
+                    <IconButton
+                      color='Green'
+                      onClick={() => setEdit(td)}
+                      disabled={Boolean(edit)}
+                      aria-label='Edit to-do entry'
+                    >
+                      <Edit aria-hidden={true} />
+                    </IconButton>
+                    <ValidatedCheckboxInput
+                      className='ml-2'
+                      name='_action'
+                      label='Complete to-do entry'
+                      value='complete'
+                      submitOnChange={true}
+                      disabled={loadingContext.isLoading}
+                    />
+                  </div>
+                )}
+              </Panel>
+            </ValidatedForm>
           ))}
         </Panel>
-        <UpsertTodo
-          todo={edit}
+
+        <ValidatedForm
+          validator={validator}
           onSubmit={() => {
             setTimeout(clearEdit)
           }}
-          disabled={loadingContext.isLoading}
-        />
+          resetAfterSubmit={true}
+          method='post'
+        >
+          <ValidatedHiddenInput name='id' value={edit?.id.toString()} />
+          <div className='mt-2 py-3 px-4 grid grid-flow-col auto-cols-[1fr_200px] gap-2 items-start'>
+            <ValidatedTextInput
+              className='p-2 border'
+              label='To-do description'
+              placeholder='Todo description'
+              name='description'
+              value={edit?.description}
+              disabled={loadingContext.isLoading}
+            />
+            <Button
+              className='text-black'
+              type='submit'
+              name='_action'
+              value='upsert'
+              disabled={loadingContext.isLoading}
+            >
+              {edit ? 'Edit' : 'Add'}
+            </Button>
+          </div>
+        </ValidatedForm>
       </GlassPanel>
     </div>
   )
